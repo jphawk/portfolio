0c6af517eacf40d18e7bc8ae34bc9fb68fce637a
Elena Kazakova <jphawk@Elenas-MBP.home>
2020-04-12 13:12:04 +0300

    Initializing project

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+// ==========================================================================
+// Layout components
+// ==========================================================================
+
+import About from "./header";
+import Header from "./header";
+import Hero from "./hero";
+import Follow from "./follow";
+import Footer from "./footer";
+import Logo from "./logo";
+
+export { About, Header, Hero, Follow, Footer, Logo };
